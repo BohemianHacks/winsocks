be731c62b4182e8321827a845a4dc504611cd24d
BohemianHacks <jeff.blecha@gmail.com>
2013-12-28 22:41:35 -0600

    Update main.c

--- main.c
+++ main.c
@@ -8,13 +8,21 @@ int main(int argc, char**argv){
 const int iReqWinsockVer = 2;   // Minimum winsock version required
 
 WSADATA wsaData;
+SOCKET hSocket;
 
 if (WSAStartup(MAKEWORD(iReqWinsockVer,0), &wsaData)==0)
 {
     // Check if major version is at least iReqWinsockVer
     if (LOBYTE(wsaData.wVersion) >= iReqWinsockVer)
     {
-        printf("Launched\n");
+        hSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+        if (hSocket==INVALID_SOCKET){
+            printf("Socket failed\n")
+        }
+        else{
+            prinf("Socket Launched\n")
+            closesocket(hSocket);
+        }
     }
     else
     {
