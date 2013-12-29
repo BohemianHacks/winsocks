89692d06c2ae81a78c90cb4df1b7e4069e1c9043
BohemianHacks <jeff.blecha@gmail.com>
2013-12-28 22:34:32 -0600

    Update main.c

--- main.c
+++ main.c
@@ -2,7 +2,34 @@
 #include <windows.h>
 #include <winsock.h>
 
-int main(int argc, char** argv){
-    printf("Test started\n");
-    return 0;
+
+
+int main(int argc, char**argv){
+const int iReqWinsockVer = 2;   // Minimum winsock version required
+
+WSADATA wsaData;
+
+if (WSAStartup(MAKEWORD(iReqWinsockVer,0), &wsaData)==0)
+{
+    // Check if major version is at least iReqWinsockVer
+    if (LOBYTE(wsaData.wVersion) >= iReqWinsockVer)
+    {
+        printf("Launched\n")
+    }
+    else
+    {
+        printf("Required version not available");
+    }
+
+    // Cleanup winsock
+    if (WSACleanup()!=0)
+    {
+        printf("cleanup failed\n");
+    }
+}
+else
+{
+    printf("startup failed\n");
+}
+return 0;
 }
